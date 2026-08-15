c4c9c840882f3ec3aaf91013016f356df4b1e15f
omkar ven <omkarven05@gmail.com>
2026-08-15 13:01:14 +0530

    changed resources

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import API_BASE_URL from "../config";
+import AnalyticsDashboard from "./AnalyticsDashboard";  
 
 export default function AdminDashboard() {
   const { user } = useAuth();
