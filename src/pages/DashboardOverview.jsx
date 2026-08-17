fd9baf9f38eeadc44dbc9b87cfbf4bcd46eaf3f5
omkar ven <omkarven05@gmail.com>
2026-08-17 16:33:21 +0530

    changed resources

--- src/pages/DashboardOverview.jsx
+++ src/pages/DashboardOverview.jsx
@@ -21,11 +21,12 @@ import Slider2 from "../assets/Slider2.png";
 import Slider3 from "../assets/Slider3.png";
 import Slider4 from "../assets/Slider4.png";
 import Slider5 from "../assets/Slider5.png";
+import Slider5 from "../assets/Slider6.jpg";
 import DiscoverRatnagiri from "./DiscoverRatnagiri";   // 👈 add this line
 
 
 
-const heroImages = [Slider1, Slider2, Slider3, Slider4, Slider5];
+const heroImages = [Slider1, Slider2, Slider3, Slider4, Slider5, Slide6];
 
 // lucide-react no longer ships trademarked brand icons (Instagram, Facebook,
 // Twitter, YouTube, etc). These small inline SVGs are drop-in replacements
